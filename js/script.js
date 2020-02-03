a978e301af6437630d92a71531877060ac3fc595
Jahirul <j.islam.trine@gmail.com>
2020-02-03 22:48:15 +0600

    The task counter is now functional; changed task deletion function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
+//event handler of load time setup
 document.addEventListener('DOMContentLoaded', initialSetup);
 
+//set the initial view and some valuues
 function initialSetup()
 {
+    if(localStorage.getItem('currentList')=== null)
+    {
+        localStorage.setItem('currentList', JSON.stringify([ ]) );
+    }
+    document.querySelector('#numberOfCompletedTasks').innerText = '0';
     taskCounter();
+    viewList();
 }
 
 //counts the number of tasks
 function taskCounter()
 {       
-        //set both value to zero
-        //get number of deleted elements
-        document.querySelector('#numberOfCompletedTasks').innerText = List.querySelectorAll('del').length;
-        //get number of all elements
-        document.querySelector('#numberOfTotalTasks').innerText = List.childElementCount;
+       let listLength = JSON.parse(localStorage.getItem('currentList')).length;
+       document.querySelector('#numberOfTotalTasks').innerText = listLength;
 }
 
 //selectors
 const submitButton = document.querySelector('#submitButton');
-const List = document.querySelector('ul#currentList');
 
 //add task
 submitButton.addEventListener('click', addTask);
@@ -26,15 +30,34 @@ function addTask(e)
 {
     e.preventDefault();
     let inputBox = document.querySelector('#textInput');
-    let currentList = document.querySelector('.list');
     let task = inputBox.value;
     inputBox.value = ' ';
+    let storedList = JSON.parse(localStorage.getItem('currentList'));
+    storedList.push(task);
+    localStorage.setItem('currentList', JSON.stringify(storedList));
+    taskCounter();
     let newElement = elementCreator('li', task, 'list-item');
-    let deleteButton = elementCreator('button', 'delete', 'button is-small is-pulled-right deleteButtons');
+    let deleteButton = elementCreator('button',
+     'delete', 'button is-small is-pulled-right deleteButtons');
     deleteButton.addEventListener('click', deleteTask);
     newElement.appendChild(deleteButton);
+    let currentList = document.querySelector('#currentList');
     currentList.appendChild(newElement);
     taskCounter();
+}
+
+//view tasks
+function viewList()
+{
+    let currentList = document.querySelector('.list');
+    let storedList = JSON.parse(localStorage.getItem('currentList'));
+    storedList.forEach(function (task){
+        let newElement = elementCreator('li', task, 'list-item');
+        let deleteButton = elementCreator('button', 'delete', 'button is-small is-pulled-right deleteButtons');
+        deleteButton.addEventListener('click', deleteTask);
+        newElement.appendChild(deleteButton);
+        currentList.appendChild(newElement);
+    });
 }
 
 
@@ -45,7 +68,10 @@ function deleteTask(e)
         let deletedElement = elementCreator('del', taskTextContent, 'deletedTask is-block');
         let parent = e.target.parentElement.parentElement;
         parent.replaceChild(deletedElement, e.target.parentElement);
-        taskCounter();
+        let newList = JSON.parse(localStorage.getItem('currentList'));
+        newList.splice(newList.indexOf(taskTextContent), 1);
+        localStorage.setItem('currentList', JSON.stringify(newList));
+        document.querySelector('#numberOfCompletedTasks').innerText = String(Number(document.querySelector('#numberOfCompletedTasks').innerText)+1); 
     }
 
 //element creator
